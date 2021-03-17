67e86880f2e14b8746a94d217d05bd06d27cfbac
Bair <liberty2217@yandex.ru>
2021-03-17 18:02:21 +0300

    feat: update

--- src/count-cats.js
+++ src/count-cats.js
@@ -4,6 +4,8 @@ module.exports = function countCats( matrix ){
   
   let result = 0;
 
+  
+
   for (let i = 0; i < matrix.length; i = i + 1){
     if (matrix[i] === '^^'){
     result = result + 1;
