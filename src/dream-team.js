e9f5b8d05e8de1a09ca79f75ab72b246ab140ec6
Bair <liberty2217@yandex.ru>
2021-03-18 13:27:40 +0300

    refractor: dream-team solved

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,41 +1,36 @@
-
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-    //throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-  if(!Array.isArray(members)){
+    
+  //checking that passed argument is always array data type;
+  if (!Array.isArray(members)) {
     return false;
   }
 
+  //since the passed array includes all data types - save in variable only string 
   let nameOfdreamTeam = [];
-
-  for(elem of members){
+  for (elem of members) {
     if (typeof elem !='string') {
       continue;
     } else {
     nameOfdreamTeam.push(elem);
     }
   }
-
-  let cut = nameOfdreamTeam.map(function(name) {
-    return name[0];
-  }).sort(function(a, b) {
-    a.localeCompare(b);
-  })
-  return console.log(cut);
-
-
-
-}
-//     let temp = firstLetterOfName(elem);
-//     dreamTeam+=temp==-1 ? '' : temp;
-//   }
   
-//   return dreamTeam.split('').sort().join('');
-// };
+  //configure obtained array of string by alphabetical sort and leaving only first letters
+  let resultArray = nameOfdreamTeam.map(takeFirstLetter).sort(alphabeticalSort)
 
-// function firstLetterOfName(elem){
-//   elem=elem.replace(/\s/g,''); //get rid of whitespases
-//   return elem.length>0 ? elem[0].toUpperCase() : -1;
-// }
+  return resultArray.join('');
+}
+
+function takeFirstLetter(name) {
+  //since string data type can include space as a first symbol - remove all spaces in names
+  let removedSpace = name.replace(/\s/g,'');
+  return removedSpace[0].toUpperCase();
+}
+
+function alphabeticalSort(curName, nextName) {
+    if (curName < nextName) { return -1; }
+    if (curName > nextName) { return 1; }
+    return 0;
+}
