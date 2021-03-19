45a4b7d1d5bf2d09266b947bd12017e387461135
Bair <liberty2217@yandex.ru>
2021-03-19 12:21:41 +0300

    feat: add remak

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -5,6 +5,8 @@ module.exports = class DepthCalculator {
     if (!Array.isArray(arr)){
     	return 0;
     }
+    // we do need counter since if we obtain array - we always return at least 1
+    // the total number of nested arrays will be calculated in num var
     let counter = 1;
     for (let el of arr) {
       let num = 0;
